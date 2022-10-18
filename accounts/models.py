e2033e223870e2de93dac339e49a8e5477b881ce
Simon <sialvarezve@unal.edu.co>
2022-10-18 06:23:45 -0500

    Create employee template

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    python_skill = models.IntegerField(default=0)
+    sql_skill = models.IntegerField(default=0)
+    java_skill = models.IntegerField(default=0)
+    spark_skill = models.IntegerField(default=0)
+    html_css_skill = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
