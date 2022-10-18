d6fe76d28acf5bbf91c0a77931c3cf8554a0f9a6
Simon <sialvarezve@unal.edu.co>
2022-10-18 06:23:17 -0500

    Create employee template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, ProfilesListView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('profilesList/', ProfilesListView.as_view(), name='profilesList'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .models import Employee
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class ProfilesListView(generic.ListView):
+    model = Employee
+    # template_name = '/accounts/employee_list.html'
