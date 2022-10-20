1636ca3fa321f36f1fd9452f124633eba6cd200d
Simon <sialvarezve@unal.edu.co>
2022-10-20 06:28:18 -0500

    Adding chart capability

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from plotly.offline import plot
+import plotly.graph_objects as go
 import hashlib
 from .models import Employee
 
@@ -12,8 +14,101 @@ class SignUpView(generic.CreateView):
 
 class EmployeesListView(generic.ListView):
     model = Employee
-    # template_name = '/accounts/employee_list.html'
 
 class EmployeeDetailView(generic.DetailView):
     model = Employee
-    # template_name = '/accounts/employee_detail.html'
+
+    def demo_plot_view(request):
+        """ 
+        View demonstrating how to display a graph object
+        on a web page with Plotly. 
+        """
+        
+        # Generating some data for plots.
+        x = [i for i in range(-10, 11)]
+        y1 = [3*i for i in x]
+        y2 = [i**2 for i in x]
+        y3 = [10*abs(i) for i in x]
+
+        # List of graph objects for figure.
+        # Each object will contain on series of data.
+        graphs = []
+
+        # Adding linear plot of y1 vs. x.
+        graphs.append(
+            go.Scatter(x=x, y=y1, mode='lines', name='Line y1')
+        )
+
+        # Adding scatter plot of y2 vs. x. 
+        # Size of markers defined by y2 value.
+        graphs.append(
+            go.Scatter(x=x, y=y2, mode='markers', opacity=0.8, 
+                    marker_size=y2, name='Scatter y2')
+        )
+
+        # Adding bar plot of y3 vs x.
+        graphs.append(
+            go.Bar(x=x, y=y3, name='Bar y3')
+        )
+
+        # Setting layout of the figure.
+        layout = {
+            'title': 'Title of the figure',
+            'xaxis_title': 'X',
+            'yaxis_title': 'Y',
+            'height': 420,
+            'width': 560,
+        }
+
+        # Getting HTML needed to render the plot.
+        plot_div = plot({'data': graphs, 'layout': layout}, 
+                        output_type='div')
+
+        return plot_div
+
+    # override context data
+    def get_context_data(self, *args, **kwargs):
+        context = super(EmployeeDetailView, self).get_context_data(*args, **kwargs)
+        # add extra field 
+        # Generating some data for plots.
+        x = [i for i in range(-10, 11)]
+        y1 = [3*i for i in x]
+        y2 = [i**2 for i in x]
+        y3 = [10*abs(i) for i in x]
+
+        # List of graph objects for figure.
+        # Each object will contain on series of data.
+        graphs = []
+
+        # Adding linear plot of y1 vs. x.
+        graphs.append(
+            go.Scatter(x=x, y=y1, mode='lines', name='Line y1')
+        )
+
+        # Adding scatter plot of y2 vs. x. 
+        # Size of markers defined by y2 value.
+        graphs.append(
+            go.Scatter(x=x, y=y2, mode='markers', opacity=0.8, 
+                    marker_size=y2, name='Scatter y2')
+        )
+
+        # Adding bar plot of y3 vs x.
+        graphs.append(
+            go.Bar(x=x, y=y3, name='Bar y3')
+        )
+
+        # Setting layout of the figure.
+        layout = {
+            'title': 'Title of the figure',
+            'xaxis_title': 'X',
+            'yaxis_title': 'Y',
+            'height': 420,
+            'width': 560,
+        }
+
+        # Getting HTML needed to render the plot.
+        plot_div = plot({'data': graphs, 'layout': layout}, 
+                        output_type='div')
+
+        context["spider_chart"] = plot_div
+        return context                    
